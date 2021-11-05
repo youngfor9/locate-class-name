48160bb19b5e13b9f3be872f0ea2d8e005d367fa
GitHub Action <action@github.com>
2021-11-05 09:14:31 +0000

    Template cleanup

--- src/main/kotlin/com/github/youngfor9/locateclassname/MyBundle.kt
+++ src/main/kotlin/com/github/youngfor9/locateclassname/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.youngfor9.locateclassname
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/youngfor9/locateclassname/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/youngfor9/locateclassname/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.youngfor9.locateclassname.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.youngfor9.locateclassname.MyBundle
 
 class MyApplicationService {
 
